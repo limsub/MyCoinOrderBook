5f55474e28118dee8947f6cb92fb175a72a19a1c
limsss9076@naver.com <limsss9076@naver.com>
2023-12-23 10:14:25 +0900

    [Horizontal] UI 작업 완ㄹ

--- MyCoinOrderBook/MyCoinOrderBook/Scene/Horizontal/HorizontalView.swift
+++ MyCoinOrderBook/MyCoinOrderBook/Scene/Horizontal/HorizontalView.swift
@@ -13,93 +13,102 @@ struct HorizontalView: View {
     
     
     var body: some View {
-        ScrollView {
-            
-            Text(viewModel.marketData.koreanName)
-                .frame(height: 40)
-            
-            // GoemetryReader : 그 자체로 'View'이며, container 안의 View 스스로의 크기와 위치를 함수로 정의한다
-            // '상위 View가 GeometryReader에게 제안한 위치, 크기에 대한 정보에 접근할 수 있게 한다'
-            // proxy : GeometryReader에게 할당된 크기, 위치를 뜻한다.
-            
-            
-            GeometryReader { proxy in
-                
-                let graphWidth = proxy.size.width * 0.6 // 기기 사이즈의 0.6 만큼 쓰겠다
-                
-                VStack {
-                    // 1. ask
-                    ForEach(viewModel.askOrderBook, id: \.self) { item in
-                        
-                        HStack {
-                            Text("\(item.price.formatted())")
-                            
-//                            Text("\(item.data)")
-                                .frame(width: proxy.size.width * 0.4)
-                            
-                            ZStack(alignment: .leading) {
-                                
-                                let graphSize = CGFloat(item.size) / CGFloat(viewModel.largestAskSize()) * graphWidth
-                                
-                                Rectangle()
-                                    .foregroundColor(.blue.opacity(0.4))
-                                    .frame(maxWidth: graphSize, alignment: .leading)
-                                
-                                Text("\(item.size)")
-                                    .frame(width: graphWidth)
-                                
-                            }
-                            .frame(width: .infinity)
-                            .background(.gray)
-                                       
-                        }
-                        .frame(height: 40)
-                        
-                    }
-                    
-                    Spacer()
-                        .frame(height: 40)
-                        .background(.yellow)
-                    
-                    // 2. bid
-                    ForEach(viewModel.bidOrderBook, id: \.self) { item in
-                        
-                        HStack {
-                            Text("\(item.price.formatted())")
-                            
-//                            Text("\(item.data)")
-                                .frame(width: proxy.size.width * 0.4)
-                            
-                            ZStack(alignment: .leading) {
-                                
-                                let graphSize = CGFloat(item.size) / CGFloat(viewModel.largestBidSize()) * graphWidth
-                                
-                                Rectangle()
-                                    .foregroundColor(.blue.opacity(0.4))
-                                    .frame(maxWidth: graphSize, alignment: .leading)
-                                
-                                Text("\(item.size)")
-                                    .frame(width: graphWidth)
-                                
-                            }
-                            .frame(width: .infinity)
-                            .background(.gray)
-                                       
-                        }
-                        .frame(height: 40)
-                        
-                    }
-                    
-                   
-                }
-                .background(.red)
-            }
-            .frame(height : 1440) // 40 * 15 * 2 + 40 + 40
-            
-        }
-        .onAppear {
-            viewModel.fetchOrderBook()
+        NavigationStack {
+            ScrollView {
 
+                // GoemetryReader : 그 자체로 'View'이며, container 안의 View 스스로의 크기와 위치를 함수로 정의한다
+                // '상위 View가 GeometryReader에게 제안한 위치, 크기에 대한 정보에 접근할 수 있게 한다'
+                // proxy : GeometryReader에게 할당된 크기, 위치를 뜻한다.
+                
+                
+                GeometryReader { proxy in
+                    
+                    let graphWidth = proxy.size.width * 0.3 // 기기 사이즈의 0.6 만큼 쓰겠다
+                    
+                    VStack(alignment: .center) {
+                        // 1. ask
+                        ForEach(viewModel.askOrderBook, id: \.self) { item in
+                            
+                            HStack(alignment: .center) {
+                                // 1.
+                                ZStack(alignment: .trailing) {
+                                    let graphSize = CGFloat(item.size) / CGFloat(viewModel.largestAskSize()) * graphWidth
+                                    
+                                    Rectangle()
+                                        .foregroundColor(Color(hexString: ConstantColor.blueGraphColor.hexCode))
+                                        .frame(maxWidth: graphSize, alignment: .trailing)
+                                        .frame(height: 20)
+                                    
+                                    Text("\(item.size)")
+                                        .frame(width: graphWidth)
+                                    
+                                }
+                                .frame(maxWidth: graphWidth)
+                                .frame(height: 40)
+                                .background(Color(hexString: ConstantColor.blueBackgroundColor.hexCode))
+                                
+                                // 2.
+                                Text("\(item.price.formatted())")
+                                    .frame(width: proxy.size.width * 0.3)
+                                    .frame(height: 40)
+                                    .foregroundStyle(Color(hexString: ConstantColor.blueTextColor.hexCode))
+                                    .background(Color(hexString: ConstantColor.blueBackgroundColor.hexCode))
+                                
+                                // 3.
+                                Spacer()
+                                    .frame(width: proxy.size.width * 0.3)
+                            }
+                            .frame(height: 40)
+                            
+                        }
+                        
+                        
+                        // 2. bid
+                        ForEach(viewModel.bidOrderBook, id: \.self) { item in
+                            
+                            HStack(alignment: .center) {
+                                // 1.
+                                Spacer()
+                                    .frame(width: proxy.size.width * 0.35)
+                                
+                                // 2.
+                                Text("\(item.price.formatted())")
+                                    .frame(width: proxy.size.width * 0.3)
+                                    .frame(height: 40)
+                                    .foregroundStyle(Color(hexString: ConstantColor.blueTextColor.hexCode))
+                                    .background(Color(hexString: ConstantColor.redBackgroundColor.hexCode))
+                                
+                                // 3.
+                                ZStack(alignment: .leading) {
+                                    
+                                    let graphSize = CGFloat(item.size) / CGFloat(viewModel.largestBidSize()) * graphWidth
+                                    
+                                    Rectangle()
+                                        .foregroundColor(Color(hexString: ConstantColor.redGraphColor.hexCode))
+                                        .frame(maxWidth: graphSize, alignment: .leading)
+                                        .frame(height: 20)
+                                    
+                                    Text("\(item.size)")
+                                        .frame(width: graphWidth)
+                                    
+                                }
+                                .frame(maxWidth: graphWidth)
+                                .frame(height: 40)
+                                .background(Color(hexString: ConstantColor.redBackgroundColor.hexCode))
+                                           
+                            }
+                            .frame(height: 40)
+                        }
+                    }
+                }
+                .frame(height : 1440) // 40 * 15 * 2 + 40 + 40
+                
+            }
+            .onAppear {
+                viewModel.fetchOrderBook()
+            }
+            .navigationTitle(viewModel.marketData.koreanName)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
